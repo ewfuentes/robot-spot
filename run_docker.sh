22f2330f84e563b8cf681359e884e79a7ee41f22
Erick Fuentes <erick@csail.mit.edu>
2024-02-19 16:14:38 -0500

    add initial files

---
+++ run_docker.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+XAUTH=/tmp/.docker.xauth
+
+PWD=`pwd`
+
+echo "Preparing Xauthority data..."
+xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]; then
+    if [ ! -z "$xauth_list" ]; then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+echo "Done."
+echo ""
+echo "Verifying file contents:"
+file $XAUTH
+echo "--> It should say \"X11 Xauthority data\"."
+echo ""
+echo "Permissions:"
+ls -FAlh $XAUTH
+echo ""
+echo "Running docker..."
+
+xhost +local:docker
+docker build -t robot-spot:latest .
+
+# Original command (broke)
+docker run -it --net=host --gpus all \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH:rw" \
+    --volume="$PWD:/host"\
+    --volume="$HOME/.ssh:/home/robot/.ssh"\
+    --privileged \
+    --user="$(id -u):$(id -g)" \
+    robot-spot:latest \
+    /bin/bash
---
+++ run_me_in_docker.sh
@@ -0,0 +1,11 @@
+#! /bin/env bash
+
+CURDIR=$(pwd)
+
+cd $(dirname $0)/catkin_ws/src/spot_ros
+rosdep install --from-paths spot_cam spot_description spot_driver \
+    spot_msgs spot_viz spot_wrapper --ignore-src -y
+cd spot_wrapper
+pip install .
+
+cd $CURDIR
