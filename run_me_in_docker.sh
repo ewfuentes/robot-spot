11013233917cc3d67aba65541954b6ae8a8ae664
Erick Fuentes <erick@csail.mit.edu>
2024-02-21 14:19:29 -0500

    add apriltag tracking

--- run_me_in_docker.sh
+++ run_me_in_docker.sh
@@ -2,6 +2,7 @@
 
 CURDIR=$(pwd)
 
+rosdep install brm_evanescence
 cd $(dirname $0)/catkin_ws/src/spot_ros
 rosdep install --from-paths spot_cam spot_description spot_driver \
     spot_msgs spot_viz spot_wrapper --ignore-src -y
