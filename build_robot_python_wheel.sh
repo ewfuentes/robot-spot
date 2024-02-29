ff25312504b82d8864c56c77564b8382410d1cb0
Erick Fuentes <erick@csail.mit.edu>
2024-02-29 15:23:08 -0500

    pass observations into estimator

---
+++ build_robot_python_wheel.sh
@@ -0,0 +1,7 @@
+
+cd ../robot
+DAZEL_RC_FILE=".dazelrc_20.04" DAZEL_BAZEL_RC_FILE=".dazel_bazelrc" dazel build --config=python-3.8 //experimental/beacon_sim:beacon_sim_wheel
+WHEEL_NAME=`cat bazel-bin/experimental/beacon_sim/beacon_sim_wheel.name`
+cp bazel-bin/experimental/beacon_sim/$WHEEL_NAME ../robot-spot
+cd ../robot-spot
+chmod 777 $WHEEL_NAME
--- catkin_ws/src/brm_evanescence/src/estimation_node.py
+++ catkin_ws/src/brm_evanescence/src/estimation_node.py
@@ -1,40 +1,85 @@
 #! /usr/bin/env python3
+from __future__ import annotations
+
 import argparse
 import sys
+import math
 
 import rospy
 import tf2_ros
+import tf2_geometry_msgs
 from geometry_msgs.msg import TransformStamped
 from apriltag_ros.msg import AprilTagDetectionArray
 
-from typing import NamedTuple
+from experimental.beacon_sim import ekf_slam_python as esp
+from experimental.beacon_sim.generate_observations_python import BeaconObservation 
+from common.time import robot_time_python as rtp
+from common.liegroups import se2_python as se2
 
-class Observation:
-    id: int
-    body_from_tag: TransformStamped
+from typing import NamedTuple
+import numpy as np
+np.set_printoptions(linewidth=200)
+
 
 
 def compute_observations(detections, tf_buffer):
+    observations = []
     for detection in detections:
-        rospy.loginfo(detection)
-        camera_frame = detection.pose.header.frame_id
+        stamped_pose = detection.pose
+        camera_frame = stamped_pose.header.frame_id
+
         body_from_camera = tf_buffer.lookup_transform(
                 'body', camera_frame, rospy.Time.now(), rospy.Duration(1.0))
 
         id = detection.id[0]
+        stamp = stamped_pose.header.stamp
+        time_of_validity = rtp.RobotTimestamp() + rtp.as_duration(stamp.secs) + rtp.as_duration(stamp.nsecs / 1e9)
 
-        camera_from_tag = detection.pose.pose.pose
+        camera_from_tag = detection.pose.pose
 
-        rospy.loginfo('*' * 20)
-        rospy.loginfo(id)
-        rospy.loginfo(body_from_camera)
-        rospy.loginfo(camera_from_tag)
+        body_from_tag = tf2_geometry_msgs.do_transform_pose(camera_from_tag, body_from_camera)
+
+        range_m_sq = body_from_tag.pose.position.x**2 + body_from_tag.pose.position.y**2
+        range_m = math.sqrt(range_m_sq)
+        bearing_rad = math.atan2(body_from_tag.pose.position.y, body_from_tag.pose.position.x)
+
+        observations.append((time_of_validity, BeaconObservation(id, range_m, bearing_rad)))
+    return observations
 
 
-def tag_callback(detection_msg, tf_buffer):
-    observations = []
-    observations.append(
-            compute_observations(detection_msg.detections, tf_buffer))
+def tag_callback(detection_msg, tf_buffer, ekf):
+    ekf_config = esp.EkfSlamConfig(
+        max_num_beacons=2,
+        initial_beacon_uncertainty_m=100.0,
+        along_track_process_noise_m_per_rt_meter=0.1,
+        cross_track_process_noise_m_per_rt_meter=0.01,
+        pos_process_noise_m_per_rt_s=0.01,
+        heading_process_noise_rad_per_rt_meter=0.001,
+        heading_process_noise_rad_per_rt_s=0.00001,
+        beacon_pos_process_noise_m_per_rt_s=1.0,
+        range_measurement_noise_m=0.05,
+        bearing_measurement_noise_rad=0.005,
+        on_map_load_position_uncertainty_m=0.1,
+        on_map_load_heading_uncertainty_rad=0.01,
+    )
+
+    observations = compute_observations(detection_msg.detections, tf_buffer)
+    if len(observations) == 0:
+        return
+
+
+    observations = sorted(observations, key=lambda x: x[0])
+
+    for obs in observations:
+        if ekf is None:
+            ekf = esp.EkfSlam(ekf_config, obs[0])
+
+        ekf.predict(obs[0], se2.SE2(0.0))
+        ekf.update([obs[1]])
+
+    rospy.loginfo('*' * 30)
+    rospy.loginfo('estimate:')
+    rospy.loginfo(ekf.estimate().mean)
 
         
 
@@ -45,12 +90,15 @@ def main():
     tf_buffer = tf2_ros.Buffer()
     tf_listener = tf2_ros.TransformListener(tf_buffer)
     tag_detection_subscribers = []
+
+    ekf = None
+
     for camera in ['frontleft', 'frontright', 'left', 'back', 'right']:
         topic_name = f"/spot/apriltag/{camera}/tag_detections"
         tag_detection_subscribers.append(
                 rospy.Subscriber(
-                    topic_name, AprilTagDetectionArray, tag_callback,
-                    callback_args=tf_buffer))
+                    topic_name, AprilTagDetectionArray, lambda data: tag_callback(data, tf_buffer, ekf)
+                    ))
 
     try:
         rospy.spin()
